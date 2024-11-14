8c9c38415409aa4e08ea17191655be3414fb4fc5
Ady Young <ady.young@equipmentshare.com>
2024-11-14 13:54:09 +0000

    connect to atlas

--- src/startup.ts
+++ src/startup.ts
@@ -1,16 +1,24 @@
 import { MongoMemoryReplSet } from "mongodb-memory-server";
 
 import mongoose from "mongoose";
+import { MONGO_CONNECTION_STRING } from "./env/env";
 
 async function startInMemoryMongoDB() {
-  console.log("💾 Starting in-memory MongoDB");
-  const replset = await MongoMemoryReplSet.create({
-    replSet: { count: 1 },
-  }); // make the count configurabel in env
-  const uri = replset.getUri();
+  if (MONGO_CONNECTION_STRING) {
+    await mongoose.connect(MONGO_CONNECTION_STRING, {
+      dbName: "es-erp",
+    });
+    console.log("Connected to atlas MongoDB");
+  } else {
+    console.log("💾 Starting in-memory MongoDB");
+    const replset = await MongoMemoryReplSet.create({
+      replSet: { count: 1 },
+    }); // make the count configurabel in env
+    const uri = replset.getUri();
 
-  await mongoose.connect(uri, {});
-  console.log("Connected to in-memory MongoDB");
+    await mongoose.connect(uri, {});
+    console.log("Connected to in-memory MongoDB");
+  }
 }
 
 export const startup = async () => {
