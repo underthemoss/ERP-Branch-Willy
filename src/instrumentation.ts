f1c3c96f79dd7c389c1929a3d627dcac328fbbad
Ady Young <ady.young@equipmentshare.com>
2025-01-31 15:22:00 +0000

    rearrange env var

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,8 @@
 export async function register() {
+  // ie - not running on the "edge"
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    process.env.ESDB_CONNECTION_STRING = `postgresql://${process.env.ESDB_USER}:${process.env.ESDB_PASSWORD}@${process.env.ESDB_HOST}:${process.env.ESDB_PORT}/equipmentshare`;
+
     await import("./system/mongo-dev-server");
     await new Promise((res) => setTimeout(res, 5000));
     await import("./system/upsertSystemTypes");
