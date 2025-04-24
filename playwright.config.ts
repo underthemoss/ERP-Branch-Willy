f90cf2b4e218ec52ea8d238d0e7a0fb2a7f58d40
Ady Young <ady.young@equipmentshare.com>
2025-04-24 11:29:58 +0100

    add playwright

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  use: {
+    baseURL: "http://localhost:3000/es-erp/",
+    headless: true,
+  },
+  webServer: {
+    command: "npm run dev",
+    port: 3000,
+    stderr: 'pipe',
+    stdout: 'pipe',
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+  ],
+});
