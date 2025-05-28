9e68bd996afc6b0f118839c6b1362bd7b0a68dba
Ady Young <ady.young@equipmentshare.com>
2025-05-28 15:45:09 +0100

    improve tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  expect: {
+    timeout: 15000,
+  },
   use: {
+    actionTimeout: 10000,
+    navigationTimeout: 45000,
     baseURL: "http://localhost:3000/",
     headless: true,
   },
+  timeout: 60000,
   webServer: {
     command: "npm run dev",
     port: 3000,
+    timeout: 120000,
     stderr: "pipe",
     stdout: "pipe",
     reuseExistingServer: !process.env.CI,
