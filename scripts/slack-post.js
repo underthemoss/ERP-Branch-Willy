8feb52eee826e6021040f13b67593c9cb6969ec5
Ady Young <ady.young@equipmentshare.com>
2025-06-17 12:02:55 +0100

    fix ci

---
+++ scripts/slack-post.js
@@ -0,0 +1,40 @@
+const https = require("https");
+const fs = require("fs");
+const url = process.env.SLACK_WEBHOOK;
+
+if (!url) throw new Error("No slack endpoint provided");
+const content = fs.readFileSync("release_note.md", "utf8").toString();
+
+const data = JSON.stringify({
+  content,
+  channel: "C0912S17B9B",
+  blocks: [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "*Release Notes*\n\n• :hammer_and_wrench: Fixed CI workflow to properly read and post release notes to Slack.",
+      },
+    },
+  ],
+});
+console.log(data);
+const req = https.request(
+  url,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },
+  },
+  (res) => {
+    res.on("data", (d) => process.stdout.write(d));
+  }
+);
+req.on("error", (error) => {
+  console.error(error);
+  process.exit(1);
+});
+req.write(data);
+req.end();
