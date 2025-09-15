517c06f0e34f72a898dee558baacbe972df643bf
Brian Mullan <brian.mullan@equipmentshare.com>
2025-09-15 17:27:36 +0100

    debug vercel (#135)
    * debug vercel
    
    * use staging target
    
    * rebuild for prod
    
    * remove my branch, ready to merge

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+console.log(
+  "NEXT_PUBLIC_AUTH0_DOMAIN at build:",
+  process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
+);
+
 const nextConfig: NextConfig = {
   basePath: "",
   assetPrefix: "",
