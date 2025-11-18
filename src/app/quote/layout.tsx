5ee18e0596086a4fb80081f6aa4aaea888ba2914
Ady Young <ady.young@equipmentshare.com>
2025-11-18 10:54:42 +0000

    lint

--- src/app/quote/layout.tsx
+++ src/app/quote/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { ReactNode } from "react";
 import { ProviderComposerNoAuth } from "@/providers/ProviderComposer";
+import { ReactNode } from "react";
 
 export default function QuoteLayout({ children }: { children: ReactNode }) {
   return <ProviderComposerNoAuth>{children}</ProviderComposerNoAuth>;
